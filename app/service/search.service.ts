f62b0b2a6fa853f89d1c348d69eeabc3b811c706
choy1379 <choy1379@gmail.com>
2017-02-18 07:15:39 +0900

    s

--- app/service/search.service.ts
+++ app/service/search.service.ts
@@ -10,37 +10,37 @@ export class searchService{
     searchDJ(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/searchDJ', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/searchDJ', params, {headers: headers})
             .map(res => res.json());
     }
     searchPlaylist(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/X-www-form-urlencoded');
-        return this._http.post('http://localhost:4100/searchPlaylist', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/searchPlaylist', params, {headers: headers})
             .map(res => res.json());
     }
     youtube_dl_one(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/X-www-form-urlencoded');
-        return this._http.post('http://localhost:4100/youtube_dl_one', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/youtube_dl_one', params, {headers: headers})
             .map(res => res.json());
     }
     youtube_dl(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/youtube_dl', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/youtube_dl', params, {headers: headers})
             .map(res => res.json());
     }
     textdownload(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/textdownload', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/textdownload', params, {headers: headers})
             .map(res => res.json());
     }
     youtube_dl_multiple(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/X-www-form-urlencoded');
-        return this._http.post('http://localhost:4100/youtube_dl_multiple', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/youtube_dl_multiple', params, {headers: headers})
             .map(res => res.json());
     }
 }
