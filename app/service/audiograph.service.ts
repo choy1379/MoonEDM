860f3d9d13296e71152686db9e107ffdd7d85a61
choy1379 <choy1379@gmail.com>
2017-02-18 09:56:11 +0900

    error
    asd

--- app/service/audiograph.service.ts
+++ app/service/audiograph.service.ts
@@ -96,11 +96,11 @@ export class AudiographService {
         console.log(`Toggling playback: ${state.playing}`);
         if(state.playing == true)
         {
-        $audiograph.play()
+        audio.play()
         }
         else
         {
-        $audiograph.pause()
+        audio.pause()
         }
       }   
       // since $audiograph needs same instance, don't lose reference
