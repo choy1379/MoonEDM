acfb1bf5f9a31c178131b534b67e82c71d169a21
choy1379 <choy1379@gmail.com>
2017-05-03 21:55:31 +0900

    0503 수정
    123

--- app/service/search.service.ts
+++ app/service/search.service.ts
@@ -10,62 +10,62 @@ export class searchService{
     searchDJ(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/searchDJ', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/searchDJ', params, {headers: headers})
             .map(res => res.json());
     }
     searchPlaylist(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/X-www-form-urlencoded');
-        return this._http.post('http://localhost:4100/searchPlaylist', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/searchPlaylist', params, {headers: headers})
             .map(res => res.json());
     }
     youtube_dl_one(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/youtube_dl_one', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/youtube_dl_one', params, {headers: headers})
             .map(res => res.json());
     }
     youtube_dl(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/youtube_dl', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/youtube_dl', params, {headers: headers})
             .map(res => res.json());
     }
     textdownload(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/textdownload', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/textdownload', params, {headers: headers})
             .map(res => res.json());
     }
     youtube_dl_multiple(params){
         var headers = new Headers();
         headers.append('Content-Type', 'application/X-www-form-urlencoded');
-        return this._http.post('http://localhost:4100/youtube_dl_multiple', params, {headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/youtube_dl_multiple', params, {headers: headers})
             .map(res => res.json());
     }
      PlaylistAdd(params){
         var headers = new Headers(); 
         headers.append('Content-Type', 'application/json')
-        return this._http.post('http://localhost:4100/PlaylistAdd',params,{headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/PlaylistAdd',params,{headers: headers})
         .map(res => res.json());
    }
     PlaylistSearch(params){
         var headers = new Headers(); 
          headers.append('Content-Type', 'application/X-www-form-urlencoded');
-        return this._http.post('http://localhost:4100/PlaylistSearch',params,{headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/PlaylistSearch',params,{headers: headers})
         .map(res => res.json());
    }
        temp(){
         var headers = new Headers(); 
          headers.append('Content-Type', 'application/X-www-form-urlencoded');
-        return this._http.post('http://localhost:4100/temp',{headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/temp',{headers: headers})
         .map(res => res.json());
    }
    PlaylistDelete(params)
    {
         var headers = new Headers(); 
          headers.append('Content-Type', 'application/json');
-        return this._http.post('http://localhost:4100/PlaylistDelete',params,{headers: headers})
+        return this._http.post('https://moonedm.herokuapp.com/PlaylistDelete',params,{headers: headers})
         .map(res => res.json());
    }
     
