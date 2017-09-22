4b92fd681e0b1cbd0a0974355bf3128a169f931b
choy1379 <choy1379@gmail.com>
2017-09-22 12:21:21 +0900

    ㄴㅇㄹ
    ㅇㄴ

---
+++ app/components/dailycharts/dailycharts.component.js
@@ -0,0 +1,98 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var store_1 = require('@ngrx/store');
+var audiograph_service_1 = require('../../service/audiograph.service');
+var bugs_service_1 = require('../../service/bugs.service');
+var auth_service_1 = require('../../service/auth.service');
+var dailyChartsComponent = (function () {
+    function dailyChartsComponent(_bugsService, store, _auth) {
+        this._bugsService = _bugsService;
+        this.store = store;
+        this._auth = _auth;
+        this.tracklist = new Array();
+        this.state$ = this.store.select('audiograph');
+    }
+    dailyChartsComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        //..
+        var result = this._bugsService.bugsCharts();
+        result.subscribe(function (x) {
+            var sortingField = "Rank";
+            x.tracklist.sort(function (a, b) {
+                return a[sortingField] - b[sortingField];
+            });
+            _this.tracklist = x;
+        });
+    };
+    dailyChartsComponent.prototype.selectAll = function () {
+        for (var i = 0; i < this.tracklist["tracklist"].length; i++) {
+            this.tracklist["tracklist"][i].selected = this.selectedAll;
+        }
+    };
+    dailyChartsComponent.prototype.checkIfAllSelected = function () {
+        this.selectedAll = this.tracklist.every(function (item) {
+            return item.selected == true;
+        });
+    };
+    dailyChartsComponent.prototype.add = function () {
+        var _this = this;
+        var album = new Array();
+        this.tracklist["tracklist"].forEach(function (element) {
+            if (element.selected == true) {
+                var albumObj = new Object();
+                albumObj['albumtitle'] = element.albumTitle;
+                albumObj['artist'] = element.artist;
+                albumObj['albumImg'] = element.albumImg; //50짜리 500으로 정규식변환..?
+                albumObj['id'] = JSON.parse(localStorage.getItem('profile')).nickname;
+                album.push(albumObj);
+            }
+        });
+        var addtrackList = this._bugsService.addtrackList(album);
+        addtrackList.subscribe(function (x) {
+            console.log(x);
+            // 0801 ~ 내일작업 플레이리스트 추가하면됨 
+            for (var i = 0; i < x.data.length; i++) {
+                var newTrack = {
+                    track: x.data[i].track,
+                    artist: x.data[i].Artist,
+                    albumImg: x.data[i].AlbumImg,
+                    videoURL: x.data[i].videoURL,
+                    frequencies: [[300, 4000], [605, 5000]]
+                };
+                _this.store.dispatch({ type: audiograph_service_1.AUDIOGRAPH_ACTIONS.ADD_TRACK, payload: newTrack });
+            }
+        });
+    };
+    dailyChartsComponent.prototype.selected = function (event) {
+        if (event.selected == true) {
+            event.selected = false;
+        }
+        else {
+            event.selected = true;
+        }
+    };
+    dailyChartsComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'dailyCharts',
+            templateUrl: 'dailyCharts.component.html',
+            styleUrls: ['dailyCharts.component.scss'],
+            providers: [audiograph_service_1.AudiographService]
+        }), 
+        __metadata('design:paramtypes', [bugs_service_1.bugsService, store_1.Store, auth_service_1.Auth])
+    ], dailyChartsComponent);
+    return dailyChartsComponent;
+}());
+exports.dailyChartsComponent = dailyChartsComponent;
+// this.store.dispatch({ type: AUDIOGRAPH_ACTIONS.TARGET_TRACK, payload: index});
+// this.store.dispatch({ type: AUDIOGRAPH_ACTIONS.TARGET_TRACK, payload:{index :index , URL : x.URL}}); 
+//# sourceMappingURL=dailycharts.component.js.map
---
+++ app/components/dailycharts/dailycharts.component.ts
@@ -0,0 +1,91 @@
+import { Component,OnInit } from '@angular/core';
+import { Router,ActivatedRoute} from '@angular/router';
+import { Http, Headers} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { AUDIOGRAPH_ACTIONS,AudiographService,IPlaylistTrack } from '../../service/audiograph.service';
+import {bugsService} from '../../service/bugs.service';
+import {Auth} from '../../service/auth.service';
+
+declare var $audiograph: any;
+@Component({
+    moduleId:module.id,
+    selector: 'dailyCharts',
+    templateUrl: 'dailyCharts.component.html',
+    styleUrls: ['dailyCharts.component.scss'],
+    providers:[AudiographService]
+})
+
+
+export class dailyChartsComponent implements OnInit {
+   state$ : Observable<any>
+   tracklist = new Array()
+   selectedAll: any;
+  constructor(public _bugsService:bugsService,private store: Store<any>,private _auth: Auth) {
+    this.state$ = this.store.select<any>('audiograph')
+   }
+  ngOnInit(){
+    //..
+        var result = this._bugsService.bugsCharts();
+              result.subscribe(x => {
+                var sortingField = "Rank";
+                x.tracklist.sort(function(a, b) { // 오름차순
+                    return a[sortingField] - b[sortingField];
+                });
+                this.tracklist = x
+            }); 
+  }
+  selectAll() {
+      for (var i = 0; i < this.tracklist["tracklist"].length; i++) {
+        this.tracklist["tracklist"][i].selected = this.selectedAll;
+      }
+    }
+  checkIfAllSelected() {
+      this.selectedAll = this.tracklist.every(function(item:any) {
+          return item.selected == true;
+        })
+    }
+  add(){
+        var album = new Array()
+        
+          this.tracklist["tracklist"].forEach(element => {
+              if(element.selected == true)
+                {
+                  var albumObj = new Object()
+                  albumObj['albumtitle'] = element.albumTitle
+                  albumObj['artist'] = element.artist
+                  albumObj['albumImg'] = element.albumImg //50짜리 500으로 정규식변환..?
+                  albumObj['id'] = JSON.parse(localStorage.getItem('profile')).nickname
+                  album.push(albumObj)
+                }
+          });
+          var addtrackList = this._bugsService.addtrackList(album)
+          addtrackList.subscribe(x =>{
+              console.log(x)
+              // 0801 ~ 내일작업 플레이리스트 추가하면됨 
+              for(var i = 0; i<x.data.length; i++)
+                {
+                  let newTrack: IPlaylistTrack = {
+                    track: x.data[i].track,
+                    artist: x.data[i].Artist,
+                    albumImg:x.data[i].AlbumImg,
+                    videoURL: x.data[i].videoURL,
+                    frequencies: [[300, 4000], [605, 5000]]
+                  };
+                  this.store.dispatch({ type: AUDIOGRAPH_ACTIONS.ADD_TRACK, payload: newTrack });
+                }
+            })
+      }
+    selected(event:any){
+      if(event.selected == true)
+        {
+          event.selected =false
+        }
+      else
+        {
+          event.selected = true
+        }
+    }
+}
+  // this.store.dispatch({ type: AUDIOGRAPH_ACTIONS.TARGET_TRACK, payload: index});
+// this.store.dispatch({ type: AUDIOGRAPH_ACTIONS.TARGET_TRACK, payload:{index :index , URL : x.URL}});
